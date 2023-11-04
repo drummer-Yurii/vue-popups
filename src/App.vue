795dd94d7259cc84f811ba9505a2cf425bca70b1
Yurii Rybachok <drummer2009@ukr.net>
2023-11-04 09:49:49 +0200

    create Popup component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  
+  <main>
+    <h1>Vue Popups</h1>
+    <button>Open Popup</button>
+    <ThePopup>
+      <h2>My Popup</h2>
+    </ThePopup>
+  </main>
 </template>
 
 <script>
-
+import ThePopup from './components/ThePopup.vue';
 export default {
   name: 'App',
   components: {
-    
+    ThePopup,
+  },
+  setup() {
+    return {
+      ThePopup,
+    }
   }
 }
 </script>
---
+++ src/components/ThePopup.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="popup">
+        <div class="popup-inner">
+            <slot />
+            <button class="popup-close">Close Popup</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ThePopup',
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
